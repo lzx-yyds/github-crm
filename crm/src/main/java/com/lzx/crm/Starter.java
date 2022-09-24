b9ba2e967b89912c9f52c7823cd3b07c129e83b2
lzx821619732 <821619732@qq.com>
2022-09-24 19:15:26 +0800

    CRM项目第二版

--- crm/src/main/java/com/lzx/crm/Starter.java
+++ crm/src/main/java/com/lzx/crm/Starter.java
@@ -13,7 +13,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @MapperScan("com.lzx.crm.dao")
 public class Starter extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        System.out.println("lizixiang");
         SpringApplication.run(Starter.class);
     }
 
